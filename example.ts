23a630bcc6b5432355251a5c56aa43f8a0d8fba8
Djiar Salim <djiar@kth.se>
2022-02-25 16:38:41 +0100

    add new project layout add default configuration file for asm compiler add small example with WASI

---
+++ example.ts
@@ -0,0 +1,9 @@
+import "wasi";
+import { Console } from "as-wasi";
+
+Console.log("Hello");
+
+
+/*export function sayHello(): number {
+    return 23;
+}*/
---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("The platform can't be run yet.");
+/*const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!' + ` ${JSON.stringify(req.query)}`);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+}) */
