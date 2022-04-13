52cb283507c8f512cbcb576e257443b0c63607d2
Djiar Salim <djiar@kth.se>
2022-04-13 08:58:46 +0200

    updated filtercodes

--- filtercode/assemblyscript/checkHour.ts
+++
@@ -1,8 +0,0 @@
-import "wasi";
-//add assemblyscript imports
-declare function currentHour(): number;
-declare function skipAction(): void;
-declare function sendSMS(): void;
-export function filterCode(): void {
-var y = currentHour();var x = 13;if (y == x) {    sendSMS();} else {    skipAction();}
-}
